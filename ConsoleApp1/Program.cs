9f814a1225e60999c46655698d57d8c6221ade09
Mpho Nethathe <mpho.nethathe@iotnxt.com>
2023-04-25 01:31:44 +0200

    file path

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace NearestVehiclePositions
         static void Main(string[] args)
         {
             // Load vehicle positions from binary file
-            var vehicles = LoadVehiclePositions("VehiclePositions.DAT");
+            var vehicles = LoadVehiclePositions();
 
             // Coordinates to find the nearest vehicle positions to
             var coordinates = new List<(double latitude, double longitude)>
@@ -30,17 +30,24 @@ namespace NearestVehiclePositions
 
             // Build k-d tree of vehicle positions
             var tree = new KdTree<double, int>(2, new DoubleMath());
+
+            if (vehicles == null)
+            {
+                throw new ArgumentException("no vehicles found");
+            }
+
+
             foreach (var vehicle in vehicles)
             {
                 tree.Add(new[] { vehicle.longitude, vehicle.latitude }, vehicle.positionId);
             }
 
             // Find nearest vehicle position to each coordinate
-            int totalCoordinates = coordinates.Count;
-            int completedCoordinates = 0;
+        
 
             Parallel.ForEach(coordinates, coordinate =>
             {
+
                 var nearest = tree.GetNearestNeighbours(new[] { coordinate.longitude, coordinate.latitude }, 1)[0];
                 var nearestVehicle = vehicles[nearest.Value];
                 Console.WriteLine($"Nearest vehicle to ({coordinate.latitude}, {coordinate.longitude}) is {nearestVehicle.vehicleRegistration} at ({nearestVehicle.latitude}, {nearestVehicle.longitude})");
@@ -49,44 +56,63 @@ namespace NearestVehiclePositions
 
         }
 
-        static List<VehiclePosition> LoadVehiclePositions(string filename)
+        static List<VehiclePosition> LoadVehiclePositions()
         {
-            var positions = new List<VehiclePosition>();
-
-            using (var file = new FileStream(filename, FileMode.Open))
-            using (var reader = new BinaryReader(file))
+            try
             {
-                while (file.Position < file.Length)
-                {
-                    var positionId = reader.ReadInt32();
-                    var vehicleRegistration = ReadNullTerminatedString(reader);
-                    var latitude = reader.ReadSingle();
-                    var longitude = reader.ReadSingle();
-                    var recordedTimeUtc = reader.ReadUInt64();
+                var positions = new List<VehiclePosition>();
 
-                    positions.Add(new VehiclePosition
+                var inputFilePath = @"C:\Users\MphoNethathe\Desktop\Data\VehiclePositions.DAT";
+
+                using (var file = new FileStream(inputFilePath, FileMode.Open))
+                using (var reader = new BinaryReader(file))
+                {
+                    while (file.Position < file.Length)
                     {
-                        positionId = positionId,
-                        vehicleRegistration = vehicleRegistration,
-                        latitude = latitude,
-                        longitude = longitude,
-                        recordedTimeUtc = recordedTimeUtc
-                    });
+                        var positionId = reader.ReadInt32();
+                        var vehicleRegistration = ReadNullTerminatedString(reader);
+                        var latitude = reader.ReadSingle();
+                        var longitude = reader.ReadSingle();
+                        var recordedTimeUtc = reader.ReadUInt64();
+
+                        positions.Add(new VehiclePosition
+                        {
+                            positionId = positionId,
+                            vehicleRegistration = vehicleRegistration,
+                            latitude = latitude,
+                            longitude = longitude,
+                            recordedTimeUtc = recordedTimeUtc
+                        });
+                    }
                 }
+
+                return positions;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error {ex.Message} ");
             }
 
-            return positions;
+            return  null;
         }
 
         static string ReadNullTerminatedString(BinaryReader reader)
         {
-            var bytes = new List<byte>();
-            byte b;
-            while ((b = reader.ReadByte()) != 0)
+            try
             {
-                bytes.Add(b);
+                var bytes = new List<byte>();
+                byte b;
+                while ((b = reader.ReadByte()) != 0)
+                {
+                    bytes.Add(b);
+                }
+
+                return System.Text.Encoding.ASCII.GetString(bytes.ToArray());
             }
-            return System.Text.Encoding.ASCII.GetString(bytes.ToArray());
+            catch (Exception ex)
+            {
+                throw new Exception("error");
+            }
         }
     }
 
